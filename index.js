a06141784694552a922b20c93ed1f1ae47a241a3
J0vvy <154846239+J0vvy@users.noreply.github.com>
2024-06-04 12:39:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const { Console } = require("console");
+
+
+//Power Number Array
+var inputNumber = [9];
+
+var Powers = [1,2,4,8,16,32,64]
+
+var InsertArray = Powers.concat(inputNumber);
+
+//Input Identifier
+
+Identifier = InsertArray.length - 1;
+
+//Sort Input Number into Powers
+
+
+console.log('comparing ', inputNumber, ' with each instance of ', Powers);
+
+for(i = 0; i < Powers.length; i++){
+ 
+
+   
+  //condition checker if less/more than.
+  do{
+    
+  }
+ while (InsertArray.at(Identifier) < InsertArray.at(i)){
+    var counter = i + 0;
+   
+   var morethan = InsertArray.at(i);
+   
+   console.log('counter', counter);
+   console.log(morethan);
+   //print the first last instance of the less than.
+ }
+
+}
+
+let SortArray = Powers.insert(2, 1);
+  
+var NewArray = 1;
+console.log('outside loop', counter);
+console.log(NewArray);
+
+console.log(Powers);
+ 
+  
+
+
+
